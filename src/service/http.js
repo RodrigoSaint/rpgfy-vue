d407d285829957866ee618ec8a702be050b23eeb
Rodrigo Santos da Silva <rodrigo.silva@esx.com.br>
2018-01-05 17:04:25 -0200

    adding deployment script

--- src/service/http.js
+++ src/service/http.js
@@ -4,7 +4,8 @@ import CredentialService from "./credential";
 export default function GetHttp() 
 {
     // var axiosOption = {baseURL: 'https://59f246f5a118a000126fbe97.mockapi.io/' };
-    var axiosOption = {baseURL: 'http://localhost:3000/' };
+    //var axiosOption = {baseURL: 'http://localhost:3000/' };
+    var axiosOption = {baseURL: 'https://rpgfy-node.azurewebsites.net/' };    
     var credential = CredentialService.getCredential();
     if(credential) axiosOption.headers = { 'X-Auth-Token': credential }
     return axios.create(axiosOption);
